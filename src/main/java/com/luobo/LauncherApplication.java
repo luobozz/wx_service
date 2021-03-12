8500eea7e8edf620f202796630ceadc44cfffd69
luobozz <437337961@qq.com>
2021-03-12 17:37:39 +0800

    初始化2

--- src/main/java/com/luobo/LauncherApplication.java
+++ src/main/java/com/luobo/LauncherApplication.java
@@ -1,7 +1,7 @@
 package com.luobo;
 
-import com.luobo.log.Log;
 import com.luobo.utils.TimeStampHelper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,13 +12,14 @@ import org.springframework.scheduling.annotation.EnableAsync;
  * @author chenlingyu
  *  @date 2020/4/26 9:57
  */
+@Slf4j
 @SpringBootApplication
 @EnableAsync
 public class LauncherApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(LauncherApplication.class, args);
-        Log.info("当前服务地址："+"http://localhost:1000/");
-        Log.info("启动成功,当前时间："+ TimeStampHelper.getNow().toString() +",当前服务地址："+"http://localhost:1000/");
+        log.info("当前服务地址："+"http://localhost:1000/");
+        log.info("启动成功,当前时间："+ TimeStampHelper.getNow().toString() +",当前服务地址："+"http://localhost:1000/");
     }
 }
---
+++ src/main/java/com/luobo/controller/WxNotifyController.java
@@ -0,0 +1,35 @@
+package com.luobo.controller;
+
+import com.luobo.query.wx.WxNotifyPortalQuery;
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.mp.api.WxMpService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * @author chenlingyu
+ */
+@Slf4j
+@RestController
+@RequestMapping("/wx/notify")
+public class WxNotifyController {
+    @Autowired
+    private WxMpService wxService;
+    @GetMapping(value="portal",produces = "text/plain;charset=utf-8")
+    public String getPortal(@Valid WxNotifyPortalQuery query){
+        log.info("\n接收到来自微信服务器的认证消息：[{}, {}, {}, {}]", query.getSignature(),
+                query.getTimestamp(), query.getNonce(), query.getEchostr());
+        if (!this.wxService.switchover(query.getAppId())) {
+            throw new IllegalArgumentException(String.format("未找到对应appid=[%s]的配置，请核实！", query.getAppId()));
+        }
+
+        if (wxService.checkSignature(query.getTimestamp(), query.getNonce(), query.getEchostr())) {
+            return query.getEchostr();
+        }
+
+        return "非法请求";
+    }
+}
