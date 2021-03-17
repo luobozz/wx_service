fc84e86c584451052d395cd5d18b2aa9f6fef009
shtfAdmin/chenlingyu <cly@it-fly.cn>
2021-03-17 12:06:16 +0800

    运维测试

--- src/main/java/com/luobo/controller/WxNotifyController.java
+++ src/main/java/com/luobo/controller/WxNotifyController.java
@@ -16,19 +16,19 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/wx/notify")
 public class WxNotifyController {
-    @Autowired
-    private WxMpService wxService;
+//    @Autowired
+//    private WxMpService wxService;
     @GetMapping(value="portal",produces = "text/plain;charset=utf-8")
     public String getPortal(@Valid WxNotifyPortalQuery query){
         log.info("\n接收到来自微信服务器的认证消息：[{}, {}, {}, {}]", query.getSignature(),
                 query.getTimestamp(), query.getNonce(), query.getEchostr());
-        if (!this.wxService.switchover(query.getAppId())) {
-            throw new IllegalArgumentException(String.format("未找到对应appid=[%s]的配置，请核实！", query.getAppId()));
-        }
-
-        if (wxService.checkSignature(query.getTimestamp(), query.getNonce(), query.getEchostr())) {
-            return query.getEchostr();
-        }
+//        if (!this.wxService.switchover(query.getAppId())) {
+//            throw new IllegalArgumentException(String.format("未找到对应appid=[%s]的配置，请核实！", query.getAppId()));
+//        }
+//
+//        if (wxService.checkSignature(query.getTimestamp(), query.getNonce(), query.getEchostr())) {
+//            return query.getEchostr();
+//        }
 
         return "非法请求";
     }
